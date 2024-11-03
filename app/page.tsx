c9658d960ea1387dad10a8d61103633cbf63bcda
Rohitlodhii <rohitlodhi0225@gmail.com>
2024-11-03 09:29:20 +0530

    add heropage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Header from "@/components/Header";
+import Hero from "@/components/hero";
 
 
 export default function Home() {
   return (
+    <>
     <Header/>
+    <Hero/>
+    </>
   );
 }
