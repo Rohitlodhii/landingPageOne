5a9e918973a39b07518e93c25a51158975c3642a
Rohitlodhii <rohitlodhi0225@gmail.com>
2024-11-04 12:41:19 +0530

    typo fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -118,7 +118,7 @@ const Hero = () => {
         {/* Content Over the Whole Thing */}
 
         <div className='container relative mt-16'>
-            <h1 className='text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center'>AI SEO</h1>
+            <h1 className='text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center'>HELLO</h1>
 
             <p className='text-lg md:text-xl text-white/70 mt-5 text-center max-w-xl mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptas in the SEO.
             </p>
