f5827e6e1cb4109bf3e4927b51989cdb3df44d6f
Rohitlodhii <rohitlodhi0225@gmail.com>
2024-11-04 12:38:39 +0530

    add cta animations

--- components/CalltoAction.tsx
+++ components/CalltoAction.tsx
@@ -1,19 +1,103 @@
-import React from 'react'
+"use client"
+
+import React, { RefObject, useEffect, useRef } from 'react'
 import GlowButton from './ui/button'
+
+import {motion, useMotionTemplate, useMotionValue, useScroll, useTransform} from 'framer-motion'
 
 import startBg from '@/app/assets/stars.png'
 import gridLines from '@/app/assets/grid-lines.png'
 
+
+const useRelativeMousePosition = (to : RefObject<HTMLElement>) => {
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(0);
+
+    const updateMousePosition = (event : MouseEvent) => {
+        if(!to.current) return
+        const {top , left} = to.current.getBoundingClientRect();
+
+        mouseX.set(event.x - left);
+        mouseY.set(event.y - top);
+
+    }
+
+    useEffect(()=> {
+        window.addEventListener('mouseover' , updateMousePosition);
+
+        return () => {
+            window.removeEventListener('mousemove' , updateMousePosition)
+        }
+    })
+
+    return [mouseX , mouseY];
+}
+
+
 const CalltoAction = () => {
+
+    const sectionRef = useRef<HTMLElement>(null);
+
+    const borderDivRef =  useRef<HTMLDivElement>(null);
+
+    const {scrollYProgress} = useScroll({
+        target : sectionRef ,
+        offset : ['start end' , 'end start']
+    })
+
+
+    const backgroundPositionY = useTransform(scrollYProgress , [0 ,1] , [-300 , 300]);
+
+    const [mouseX , mouseY] = useRelativeMousePosition(borderDivRef);
+
+
+    const maskImage = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px,black,transparent)`;
+
+
   return (
-    <section className='py-20 md:py-24'>
+    <section ref={sectionRef} className='py-20 md:py-24'>
         <div className='container'>
-            <div className='border border-white/15 py-24 rounded-xl overflow-hidden relative' style={{
+            <motion.div 
+
+            ref={borderDivRef}
+            
+            animate = {{
+                backgroundPositionX : startBg.width ,
+                
+            }}
+
+            transition={{
+                repeat : Infinity ,
+                duration : 60 ,
+                ease : 'linear'
+            }}
+
+            
+            className='border border-white/15 py-24 rounded-xl overflow-hidden relative group' style={{
+                backgroundPositionY ,
                 backgroundImage : `url(${startBg.src})` ,
             }}>
-                <div className='absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]' style={{
+
+                    {/* Duplication The Div below */}
+
+                <div className='absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700' style={{
                     backgroundImage : `url(${gridLines.src})` ,
                 }}></div>
+
+
+                <motion.div 
+                
+                className='absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay opacity-0 group-hover:opacity-100 ' 
+                
+                style={{
+                    maskImage,
+                    backgroundImage : `url(${gridLines.src})` ,
+                }}></motion.div>
+
+
+
+
+
 
                 <div className='relative'>
             <h2 className='text-5xl md:text-6xl max-w-sm mx-auto tracking-tighter text-center font-medium'>Ai-drivern SEO for everyone.</h2>
@@ -23,7 +107,7 @@ const CalltoAction = () => {
             <GlowButton>Join Waitlist</GlowButton>
             </div>
             </div>
-            </div>
+            </motion.div>
         </div>
     </section>
   )
